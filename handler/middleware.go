ed3357cdebef387f9f0ab1a23dade2097fb50b7a
Jonathan KUMA <jonathan.kuma@gmail.com>
2023-05-29 11:41:35 +0200

    Allow middleware to be sorted with SortableMiddleware interface (#14)

--- handler/middleware.go
+++ handler/middleware.go
@@ -5,6 +5,13 @@ type Middleware interface {
 	Wrap(Handler) Handler
 }
 
+// SortableMiddleware embeds Middleware type with a Weight method
+// in order to get sorted.
+type SortableMiddleware interface {
+	Middleware
+	Weight() int
+}
+
 // MiddlewareAware is a struct containing its own middlewares.
 type MiddlewareAware interface {
 	Middlewares() []Middleware
@@ -19,4 +26,29 @@ type WithMiddlewares struct {
 // Middlewares implements the request.MiddlewareAware interface
 func (p WithMiddlewares) Middlewares() []Middleware {
 	return p.MiddlewareList
+}
+
+// ByWeight sorts a list of Middleware by Weight
+type ByWeight []Middleware
+
+func (b ByWeight) Len() int {
+	return len(b)
+}
+
+func (b ByWeight) Less(i, j int) bool {
+	var iw, jw int
+
+	if m, ok := b[i].(SortableMiddleware); ok {
+		iw = m.Weight()
+	}
+
+	if m, ok := b[j].(SortableMiddleware); ok {
+		jw = m.Weight()
+	}
+
+	return iw < jw
+}
+
+func (b ByWeight) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
 }
